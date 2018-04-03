d72ed05a8f8539822d1e154b98545e5e946eb921
Anay Patel <anaypate@usc.edu>
2018-04-02 19:47:02 -0700

    mainframe v1

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -1,23 +1,86 @@
 package frames;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
 
-public class MainFrame extends JFrame
-{
-	 public MainFrame() {
+public class MainFrame extends JFrame {
 
-	        initUI();
-	    }
-	 
-	 private void initUI() {
+	private JPanel contentPane;
+	private JTextField txtIpAddress;
+	private JTextField txtPort;
+	private JButton btnJoin;
 
-	        add(new Board());
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public MainFrame() {
+		initUI();
+	}
+	
+	/**
+	 * Create the frame.
+	 */
+	public void initUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		txtIpAddress = new JTextField();
+		txtIpAddress.setText("IP Address");
+		GridBagConstraints gbc_txtIpAddress = new GridBagConstraints();
+		gbc_txtIpAddress.insets = new Insets(0, 0, 5, 5);
+		gbc_txtIpAddress.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtIpAddress.gridx = 5;
+		gbc_txtIpAddress.gridy = 3;
+		contentPane.add(txtIpAddress, gbc_txtIpAddress);
+		txtIpAddress.setColumns(10);
+		
+		txtPort = new JTextField();
+		txtPort.setText("Port");
+		GridBagConstraints gbc_txtPort = new GridBagConstraints();
+		gbc_txtPort.insets = new Insets(0, 0, 5, 5);
+		gbc_txtPort.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPort.gridx = 5;
+		gbc_txtPort.gridy = 4;
+		contentPane.add(txtPort, gbc_txtPort);
+		txtPort.setColumns(10);
+		
+		btnJoin = new JButton("Join");
+		GridBagConstraints gbc_btnJoin = new GridBagConstraints();
+		gbc_btnJoin.insets = new Insets(0, 0, 0, 5);
+		gbc_btnJoin.gridx = 5;
+		gbc_btnJoin.gridy = 6;
+		contentPane.add(btnJoin, gbc_btnJoin);
+	}
 
-	        setSize(400, 400);
-
-	        setTitle("Application");
-	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        setLocationRelativeTo(null);
-	    }    
 }
